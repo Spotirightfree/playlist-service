d3b3ad56a14d4991a6880be5c52ff1bd968195cb
BillyKuiper <456181@student.fontys.nl>
2023-04-28 12:18:37 +0200

    moved dockerfile

--- playlist-service/Controllers/WeatherForecastController.cs
+++ playlist-service/Controllers/WeatherForecastController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System.Text;
 
 namespace playlist_service.Controllers
 {
@@ -6,28 +8,69 @@ namespace playlist_service.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController()
         {
-            _logger = logger;
+       
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpPost]
+        [Route("SendMessageMusic")]
+        public void SendRabbitMessageMusic()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            var factory = new ConnectionFactory { HostName = "192.168.240.6" };
+            factory.UserName = "playlist-service";
+            factory.Password = "playlist-service";
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "music-service-queue",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+            const string message = "Hello World!";
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: string.Empty,
+                                 routingKey: "music-service-queue",
+                                 basicProperties: null,
+                                 body: body);
         }
+
+
+        [HttpPost]
+        [Route("SendMessageLogin")]
+        public void SendRabbitMessageLogin()
+        {
+            var factory = new ConnectionFactory { HostName = "192.168.240.6" };
+            factory.UserName = "playlist-service";
+            factory.Password = "playlist-service";
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "login-service-queue",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+            const string message = "Hello World!";
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: string.Empty,
+                                 routingKey: "login-service-queue",
+                                 basicProperties: null,
+                                 body: body);
+        }
+
+        [HttpGet]
+        [Route("Hello_World")]
+        public string HelloWorld()
+        {
+            return "Hello World from Playlist-Service";
+        }
+
     }
 }
---
+++ playlist-service/Services/RabbitMQBackgroundWorkerService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Logging;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace playlist_service.Services
+{
+    public class RabbitMQBackgroundWorkerService : BackgroundService
+    {
+        readonly ILogger<RabbitMQBackgroundWorkerService> _logger;
+        public RabbitMQBackgroundWorkerService(ILogger<RabbitMQBackgroundWorkerService> logger) 
+        {
+            this._logger = logger;
+        }
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await Task.Run(() =>
+            {
+                var factory = new ConnectionFactory { HostName = "192.168.240.6" };
+                factory.UserName = "playlist-service";
+                factory.Password = "playlist-service";
+                using var connection = factory.CreateConnection();
+                using var channel = connection.CreateModel();
+
+                channel.QueueDeclare(queue: "playlist-service-queue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                Console.WriteLine(" [*] Waiting for messages.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($" [x] Received {message}");
+                };
+                channel.BasicConsume(queue: "playlist-service-queue",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            });
+        }
+    }
+}
